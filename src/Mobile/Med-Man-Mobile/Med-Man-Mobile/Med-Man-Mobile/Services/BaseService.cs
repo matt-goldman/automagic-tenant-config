2c1b4184f59977a2260394ba39e80eeab37f975c
Matt Goldman <mattgoldman1981@gmail.com>
2020-12-19 13:54:35 +1100

    added more service definitions

--- src/Mobile/Med-Man-Mobile/Med-Man-Mobile/Med-Man-Mobile/Services/BaseService.cs
+++ src/Mobile/Med-Man-Mobile/Med-Man-Mobile/Med-Man-Mobile/Services/BaseService.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
---
+++ src/Mobile/Med-Man-Mobile/Med-Man-Mobile/Med-Man-Mobile/Services/IMedicationsService.cs
@@ -0,0 +1,14 @@
+﻿using SSW.MedMan;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedManMobile.Services
+{
+    public interface IMedicationsService
+    {
+        Task<MedicationDTO> GetMedication(int id);
+        Task<List<MedicationDTO>> GetMedications();
+        Task<int> AddMedication(MedicationDTO medication);
+        Task<bool> UpdateMedication(MedicationDTO medication);
+    }
+}
