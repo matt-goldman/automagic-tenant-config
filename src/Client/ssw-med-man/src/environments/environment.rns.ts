d61bcd8556da4c662f4510df641347a423ff798a
Matt Goldman <mattgoldman1981@gmail.com>
2020-12-30 06:29:06 +1100

    added rns angular env

---
+++ src/Client/ssw-med-man/src/environments/environment.rns.ts
@@ -0,0 +1,21 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true
+};
+
+export const B2CClientId: string = "aaa6c753-a354-4c71-a98f-bfa41cbcbe0a";
+export const B2CTenant: string = "rnsmedman";
+export const ApiBaseUri: string = "https://rnsmedman.azurewebsites.net";
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
