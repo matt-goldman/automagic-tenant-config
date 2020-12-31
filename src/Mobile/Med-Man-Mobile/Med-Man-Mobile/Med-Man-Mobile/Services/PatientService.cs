95784b572d9f1ee4c3646257054ff24f573bede7
Matt Goldman <mattgoldman1981@gmail.com>
2020-12-31 13:15:56 +1100

    added 401 retry to patients service

--- src/Mobile/Med-Man-Mobile/Med-Man-Mobile/Med-Man-Mobile/Services/PatientService.cs
+++ src/Mobile/Med-Man-Mobile/Med-Man-Mobile/Med-Man-Mobile/Services/PatientService.cs
@@ -23,6 +23,29 @@ namespace MedManMobile.Services
                 var result = await _patientsClient.PostPatientAsync(patient);
                 return result.Id;
             }
+            catch(ApiException apiEx)
+            {
+                if(apiEx.StatusCode == 401)
+                {
+                    bool reauth = await HandleUnauthorizedAsync();
+                    if(reauth)
+                    {
+                        var result = await _patientsClient.PostPatientAsync(patient);
+                        return result.Id;
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Could not reauthenticate");
+                        return 0;
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine("Unexpected API error");
+                    Debug.WriteLine(apiEx.Message);
+                    return 0;
+                }
+            }
             catch(Exception ex)
             {
                 Debug.WriteLine("Could not add patient");
@@ -33,15 +56,80 @@ namespace MedManMobile.Services
 
         public async Task<List<PatientDTO>> GetAllPAtients()
         {
-            var result = await _patientsClient.GetPatientsAsync();
+            try
+            {
+                var result = await _patientsClient.GetPatientsAsync();
 
-            return result.ToList();
+                return result.ToList();
+            }
+            catch (ApiException apiEx)
+            {
+                if (apiEx.StatusCode == 401)
+                {
+                    bool reauth = await HandleUnauthorizedAsync();
+                    if (reauth)
+                    {
+                        var result = await _patientsClient.GetPatientsAsync();
+
+                        return result.ToList();
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Could not reauthenticate");
+                        return null;
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine("Unexpected API error");
+                    Debug.WriteLine(apiEx.Message);
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Could not get patients");
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
         }
 
         public async Task<PatientDTO> GetPatient(int id)
         {
-            var result = await _patientsClient.GetPatientAsync(id);
-            return result;
+            try
+            {
+                var result = await _patientsClient.GetPatientAsync(id);
+                return result;
+            }
+            catch (ApiException apiEx)
+            {
+                if (apiEx.StatusCode == 401)
+                {
+                    bool reauth = await HandleUnauthorizedAsync();
+                    if (reauth)
+                    {
+                        var result = await _patientsClient.GetPatientAsync(id);
+                        return result;
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Could not reauthenticate");
+                        return null;
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine("Unexpected API error");
+                    Debug.WriteLine(apiEx.Message);
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Could not get patient");
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
         }
 
         public async Task<bool> UpdatePatient(PatientDTO patient)
@@ -51,7 +139,30 @@ namespace MedManMobile.Services
                 await _patientsClient.PutPatientAsync(patient.Id, patient);
                 return true;
             }
-            catch(Exception ex)
+            catch (ApiException apiEx)
+            {
+                if (apiEx.StatusCode == 401)
+                {
+                    bool reauth = await HandleUnauthorizedAsync();
+                    if (reauth)
+                    {
+                        await _patientsClient.PutPatientAsync(patient.Id, patient);
+                        return true;
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Could not reauthenticate");
+                        return false;
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine("Unexpected API error");
+                    Debug.WriteLine(apiEx.Message);
+                    return false;
+                }
+            }
+            catch (Exception ex)
             {
                 Debug.WriteLine("Could not update patient");
                 Debug.WriteLine(ex.Message);
